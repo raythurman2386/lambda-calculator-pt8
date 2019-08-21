5ede98b4eaa60d237160d895711f1472db67b7f0
raythurman2386 <raymondthurman5@gmail.com>
2019-08-21 19:43:56 -0400

    .map for the operator buttons initial setup

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 //import any components needed
+import OperatorButton from './OperatorButton';
 
 //Import your array data to from the provided data file
 import { operators } from '../../../data';
@@ -14,6 +15,9 @@ const Operators = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+      {operatorData.map(operator => {
+        <OperatorButton char={operator.char} value={operator.value} />;
+      })}
     </div>
   );
 };
