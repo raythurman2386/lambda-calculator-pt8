f7d148f8ca7a647803a860d9665e75e6e2246b2b
raythurman2386 <raymondthurman5@gmail.com>
2019-08-21 20:06:56 -0400

    added styled components and very basic styling

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
 //import any components needed
 import OperatorButton from './OperatorButton';
@@ -11,15 +12,23 @@ const Operators = () => {
   const [operatorData, setOperatorData] = useState(operators);
 
   return (
-    <div>
+    <OperatorButtonContainer>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
       {operatorData.map(operator => {
         return <OperatorButton char={operator.char} value={operator.value} />;
       })}
-    </div>
+    </OperatorButtonContainer>
   );
 };
 
 export default Operators;
+
+const OperatorButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 40px;
+  justify-content: center;
+  align-items: center;
+`;
