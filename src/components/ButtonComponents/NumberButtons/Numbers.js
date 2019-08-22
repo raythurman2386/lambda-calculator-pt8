25a39f792e523f2700b00b5ec05bb5912b4d11e1
raythurman2386 <raymondthurman5@gmail.com>
2019-08-21 20:09:32 -0400

    initial map over numbers

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -12,6 +12,9 @@ const Numbers = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+      {numbers.map(number => {
+        return <Number value={number} />;
+      })}
     </div>
   );
 };
