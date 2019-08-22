e659be90bbdc00c92c470d98b4c01164196b1e7c
Raymond Thurman <raymondthurman5@gmail.com>
2019-08-22 16:04:41 +0000

    operator and number buttons background colors

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -4,7 +4,7 @@ const OperatorButton = props => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-      <button className='btn' value={props.value}>{props.char}</button>
+      <button className='btn operatorBtn' value={props.value}>{props.char}</button>
     </>
   );
 };
