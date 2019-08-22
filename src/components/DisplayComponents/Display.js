c17f26753c66701d8727f8eaba06df573aad3a67
raythurman2386 <raymondthurman5@gmail.com>
2019-08-21 20:32:35 -0400

    all components rendering to the screen

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,12 @@
-import React from "react";
+import React from 'react';
 
-const Display = () => {
-  return <div>{/* Display any props data here */}</div>;
+const Display = props => {
+  return (
+    <div>
+      {/* Display any props data here */}
+      <input placeholder={props.total} />
+    </div>
+  );
 };
+
+export default Display;
