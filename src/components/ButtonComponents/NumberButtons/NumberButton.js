277f7ca127c193654072f51ce0d5ccca147e7d28
Raymond Thurman <raymondthurman5@gmail.com>
2019-08-22 14:32:01 +0000

    minor general btn styles

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -4,7 +4,7 @@ const NumberButton = props => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-      <button value={props.value}>{props.value}</button>
+      <button className='btn' value={props.value}>{props.value}</button>
     </>
   );
 };
