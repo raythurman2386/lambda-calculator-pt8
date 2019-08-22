085da30dd854c948435729fa701ebfa5676f680e
Raymond Thurman <raymondthurman5@gmail.com>
2019-08-22 18:11:08 +0000

    removed some excess css and removed styled component imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import './App.scss';
-import styled from 'styled-components';
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 // Logo has already been provided for you. Do the same for the remaining components
