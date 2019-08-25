f28e488af20ef9494578df193aef167c753566e9
raythurman2386 <raymondthurman5@gmail.com>
2019-08-25 19:27:22 -0400

    some logic added to the operator buttons

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
-const OperatorButton = props => {
+const OperatorButton = ({ total, setTotal, value, char }) => {
+  // handler function
+  function handleChange() {
+    if (char === '+' || '-' || 'x' || '/') {
+      return setTotal(total + value);
+    } else if (char === '=') {
+      setTotal(eval(total));
+    }
+  }
+
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-      <button className='btn operatorBtn' key={props.key} value={props.value}>{props.char}</button>
+      <button
+        className='btn operatorBtn'
+        key={char}
+        value={value}
+        onClick={() => handleChange()}
+      >
+        {char}
+      </button>
     </>
   );
 };
--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,17 +1,10 @@
 import React from 'react';
 
-const SpecialButton = ({ key, value, handleChange, total }) => {
+const SpecialButton = ({ key, value, total, setTotal }) => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-      <button
-        className='btn specialBtn'
-        onClick={() => {
-          handleChange(value);
-        }}
-        key={key}
-        value={value}
-      >
+      <button className='btn specialBtn' key={key} value={value}>
         {value}
       </button>
     </>
